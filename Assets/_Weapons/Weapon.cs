c771bdd49a7d51f099b8cc4efaedee6478a2ab7a
Lethichore <glenn.verbesselt@hotmail.com>
2017-11-28 21:07:47 +0100

    Initial commit

---
+++ Assets/_Weapons/Weapon.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Weapons
+{
+    [CreateAssetMenu(menuName = "RPG/Weapon")]
+    public class Weapon : ScriptableObject
+    {
+
+        public Transform gripTransfom;
+
+        [SerializeField] GameObject weaponPrefab;
+        [SerializeField] AnimationClip attackAnimation;
+        [SerializeField] float minTimeBetweenHits = .5f;
+        [SerializeField] float maxAttackRange = 2f;
+
+        public float GetMinTimeBetweenHits()
+        {
+            return minTimeBetweenHits;
+        }
+
+        public float GetMaxAttackRAnge()
+        {
+            return maxAttackRange;
+        }
+
+        public GameObject GetWeaponPrefab()
+        {
+            return weaponPrefab;
+        }
+
+        public AnimationClip GetAttackAnimClip()
+        {
+            RemoveAnimationEvents();
+            return attackAnimation;
+        }
+
+        // so that asset packs cannot cause bugs with predefined code.
+        private void RemoveAnimationEvents()
+        {
+            attackAnimation.events = new AnimationEvent[0];
+        }
+    }
+}
